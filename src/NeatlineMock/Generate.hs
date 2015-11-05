6654fc7f7b73bc6543f393ed1e7252ffd7cefdbc
Eric Rochester <erochest@virginia.edu>
2015-11-04 21:45:54 -0500

    Fixed bugs in data generation.

--- src/NeatlineMock/Generate.hs
+++ src/NeatlineMock/Generate.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 
 module NeatlineMock.Generate where
@@ -6,15 +7,21 @@ module NeatlineMock.Generate where
 
 import           Control.Error
 import           Control.Monad
+import           Data.Foldable
 import           Data.String
 import qualified Data.Text             as T
 import           Data.Time
 import           Database.MySQL.Simple
 import           System.Random.MWC
+-- import           Text.Groom
 
 import           NeatlineMock.Types
 import           NeatlineMock.Utils
 
+
+type RowTuple = ( Int, Int, Maybe T.Text, Maybe T.Text, Maybe T.Text
+                , Maybe T.Text, Maybe UTCTime, Maybe UTCTime
+                )
 
 generate :: Int -> T.Text -> Int -> Int -> UTCTime -> DiffTime -> ConnectInfo
          -> Script ()
@@ -28,29 +35,36 @@ generate n tableName ownerId exhibitId center dateRange cinfo = do
                   , fromString (T.unpack tableName)
                   , " (owner_id, exhibit_id, title, item_title, coverage, \
                     \widgets, start_date, end_date) VALUES \
-                    \(?, ?, ?, ?, st_geomfromtext(?), ?, ?, ?);"
+                    \(?, ?, ?, ?, GeomFromText(?), ?, ?, ?);"
                   ]
-  rows <- scriptIO . executeMany cxn q $ map toRow records
-  sPutStrLn $ "Inserted " ++ show rows ++ " records"
+      rs = map toRow records
+  rows <- foldlM (step cxn q) 0 rs
+  sPutStrLn $ "Inserted " ++ show rows ++ " records!"
+  where
+    step :: Connection -> Query -> Int -> RowTuple -> Script Int
+    step cxn q c row = scriptIO $ (c +) . fromIntegral <$> execute cxn q row
 
-toRow :: NeatlineRecord -> ( Int, Int, Maybe T.Text, Maybe T.Text, Maybe T.Text
-                           , Maybe T.Text, Maybe UTCTime, Maybe UTCTime)
-toRow = undefined
+toRow :: NeatlineRecord -> RowTuple
+toRow NLRecord{..} =
+    ( _nlOwnerId, _nlExhibitId, _nlTitle, _nlItemTitle, Just _nlGeometry
+    , _nlWidgets, _nlStartDate, _nlEndDate
+    )
 
 randomDate :: UTCTime -> UTCTime -> GenIO -> IO UTCTime
 randomDate UTCTime{utctDay=fromDay} UTCTime{utctDay=toDay} g =
     UTCTime <$> fmap toEnum (uniformR (fromEnum fromDay, fromEnum toDay) g)
-            <*> fmap toEnum (uniformR (-1, 86400) g)
+            <*> fmap (secondsToDiffTime . fromIntegral)
+                    (uniformR ((-1) :: Int, 24*60*60) g)
 
 randomRecord :: Int -> Int -> UTCTime -> DiffTime -> GenIO -> IO NeatlineRecord
 randomRecord ownerId exhibitId center dateRange g = do
-  now <- getCurrentTime
-  let title      = T.pack
-                   $ "record " ++ formatTime defaultTimeLocale "%Y-%m-%d" now
-      nullIsland = "POINT (0 0)"
-      nRange     = toEnum $ fromEnum dateRange :: NominalDiffTime
+  let nRange     = toEnum $ fromEnum dateRange :: NominalDiffTime
       (from, to) = (addUTCTime (-nRange) center, addUTCTime nRange center)
+  now <- getCurrentTime
   start  <- randomDate from to g
+  let title      = T.pack
+                   $ "record " ++ formatTime defaultTimeLocale "%Y-%m-%d" start
+      nullIsland = "POINT (0 0)"
   isSpan <- uniform g
   end    <- if isSpan
             then Just <$> randomDate start to g
